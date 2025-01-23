d2025dc56f783f679821358994986808d052190f
allan zizinga <allanzizinga@yahoo.com>
2025-01-23 04:35:16 +0100

    Setup workstation, output data on screen

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+function App() {
+  const [weight, setNewWeight] = useState("");
+  const [height, setNewHeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+
+
+  const handleWeightChange = (e)=> {
+    setNewWeight(e.target.value);
+  }
+
+  const handleHeightChange=(e)=>{
+    setNewHeight(e.target.value);
+  }
+
+  const CalculateBMI = ()=>{
+    
+      if(!weight || !height){
+        alert("Both fields needed...");
+        return;
+      }
+      
+        const heightInmeters = height / 100;
+        const bmiValue= (weight /(heightInmeters**2)).toFixed(2);
+        setBmi(bmiValue);
+      
+  };
+  
+  return (
+    <>
+    
+    <section>
+       <div className={styles.input}>
+            <label htmlFor="weight">Enter weight: </label>
+            <input type="number" value={weight} onChange={handleWeightChange} placeholder='Enter weight...'  />
+            <label htmlFor="height">Enter height in cms: </label>
+            <input type="number" value={height} onChange={handleHeightChange} placeholder='Enter height...' />
+            <button onClick={CalculateBMI} >Submit</button>
+
+            { bmi && <div><h3>Your BMI is: {bmi}</h3></div>}
+            
+       </div>
+      
+    </section>
+
+   
+    </>
+  )
+}
+
+export default App
